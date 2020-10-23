3e87006257d31e584bcdaf0bf28bcd0a7b5ca718
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-23 01:21:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,14 @@
 def reverse_each_word (string)
-  string.collect do |char|
-    string.
+  retVal = ""
+  temp = ""
+  string.each_char do |letter|
+    if(letter == ' ')
+      retVal<<(temp)+" "
+      temp = ""
+    else 
+      temp.insert(0, letter)
+    end 
+  end 
+  retVal<<(temp)
+  return retVal
 end 
